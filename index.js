a698b6b87124d517e8da18dc61c100ec9acb1cdb
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2023-02-08 15:10:26 +0000

    Core app build
    JavaScript, CSS, HTML added to core build.

---
+++ index.js
@@ -0,0 +1,37 @@
+const choresInput = document.getElementById("chores-input");
+let choresOutput = document.getElementById("chores-output");
+const addBtn = document.querySelector(".add-btn");
+const deleteBtn = document.querySelector(".delete-btn");
+
+let chores = [];
+
+// Add btn event listener
+
+addBtn.addEventListener("click", function () {
+  if (choresInput.value === "") {
+    return false;
+  }
+  if (chores.includes(choresInput.value)) {
+    choresInput.value = "";
+  } else chores.push(choresInput.value);
+  renderChores();
+  choresInput.value = "";
+});
+
+// render chore items
+
+function renderChores() {
+  let html = "";
+  chores.forEach(function (chore) {
+    html += `
+        <p class="chore-style">${chore}</p>
+        `;
+  });
+  choresOutput.innerHTML = html;
+}
+
+deleteBtn.addEventListener("click", function () {
+  choresInput.value = "";
+  chores.length = 0;
+  choresOutput.innerHTML = "";
+});
